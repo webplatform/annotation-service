d2889c7f75d0735f9e5dfa7fe478343b8055762c
Randall Leeds <tilgovi@hypothes.is>
2014-06-05 11:36:46 -0700

    Add requests_oauthlib

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(
     description='Webplatform Notes Server',
     version=0.1,
     packages=find_packages(),
+
+    install_requires=[
+        'requests_oauthlib>=0.4.0',
+    ],
+
     include_package_data=True,
     zip_safe=False,
 )
