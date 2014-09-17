1d17e1ef5a9f4a43a9b8ff8f4c98355a86e6f683
Randall Leeds <randall@bleeds.info>
2014-09-17 10:57:59 -0700

    Add wpd.js sketch

---
+++ notes_server/static/scripts/wpd.js
@@ -0,0 +1,28 @@
+configure = ['$routeProvider',
+  function  ( $routeProvider ) {
+    $routeProvider.when('/wpd/callback', {
+      resolve: {
+        channel: ['$window', function ($window) {
+          var _channel = Channel.build({
+            origin: $window.location.origin,
+            scope: 'annotator:auth',
+            window: $window.opener,
+            onReady: function () {
+              _channel.call(
+                method: 'success',
+                params: $window.oauth_status,
+                success: function () {
+                  $window.close();
+                }
+              });
+            });
+          });
+          return _channel;
+        ]
+      },
+      redirectTo: '/viewer'
+    });
+];
+
+
+angular.module('wpd.notesServer', [], configure)
