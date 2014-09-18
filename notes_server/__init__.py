4f4e6812018890d22b988247e0e04c61e88210e3
Randall Leeds <randall@bleeds.info>
2014-09-17 19:30:38 -0700

    Replace session based oauth state with TOTP
    Five minute timeout on signed timestamp for OAuth state validation.

--- notes_server/__init__.py
+++ notes_server/__init__.py
@@ -1,54 +1,70 @@
 # -*- coding: utf-8 -*-
 import json
+import datetime
 
+from annotator.auth import Consumer
+from pyramid.config import Configurator
 from pyramid.httpexceptions import HTTPFound
+from pyramid.session import signed_serialize, signed_deserialize
 from pyramid.view import view_config
 from requests_oauthlib import OAuth2Session
 from requests import Request
+
+from h.api import get_consumer
+from h.interfaces import IConsumerClass
+
 
 AUTHZ_ENDPOINT = 'https://oauth.accounts.webplatform.org/v1/authorization'
 TOKEN_ENDPOINT = 'https://oauth.accounts.webplatform.org/v1/token'
 PROFILE_ENDPOINT = 'https://profile.accounts.webplatform.org/v1/session/read'
 
 
+def consumer_factory(key, **kwargs):
+    inst = Consumer(key)
+    inst.__dict__.update(kwargs)
+    return inst
+
+
 @view_config(route_name='webplatform.login')
-@view_config(route_name='webplatform.callback', renderer='h:templates/oauth.pt')
+@view_config(route_name='webplatform.callback',
+             renderer='notes_server:templates/oauth.pt')
 def login(request):
     registry = request.registry
     settings = registry.settings
-    session = request.session
 
-    key = settings['webplatform.client_id']
-    secret = settings['webplatform.client_secret']
+    now = datetime.datetime.utcnow()
+    client = get_consumer(request)
     code = request.params.get('code')
-    state = session.get('oauth_state')
-    scope = ['session']
-    provider = OAuth2Session(key, scope=scope, state=state)
+    state = request.params.get('state')
 
     authz_endpoint = request.route_url('webplatform.authorize')
     token_endpoint = request.route_url('webplatform.token')
-    profile_endpoint = 'https://profile.accounts.webplatform.org/v1/session/read'
+
+    wp_key = settings['webplatform.client_id']
+    wp_secret = settings['webplatform.client_secret']
+
+    expected = signed_serialize(now, 'webplatform.' + client.secret)
+    provider = OAuth2Session(wp_key, scope=['session'], state=expected)
 
     if code is not None:
         try:
-            assert request.params['state'] == session['oauth_state']
+            then = signed_deserialize(state, 'webplatform.' + client.secret)
+            assert (then - now).total_seconds() <= 300
             req = Request('POST', token_endpoint,
                           data=json.dumps({
-                              'client_id': key,
-                              'client_secret': secret,
+                              'client_id': wp_key,
+                              'client_secret': wp_secret,
                               'code': code,
                           }))
             prepped = provider.prepare_request(req)
             provider.token = provider.send(prepped).json()
             provider._client.access_token = provider.token['access_token']
-            result = provider.get(profile_endpoint)
-            return dict(result=json.dumps(result))
+            return provider.get(PROFILE_ENDPOINT).json()
         except:
             pass
 
-    location, state = provider.authorization_url(authz_endpoint)
-    location = location.replace('response_type=code&', '')
-    session['oauth_state'] = state
+    location, _ = provider.authorization_url(authz_endpoint)
+    location = location.replace('response_type=code&', '')  # unused
     return HTTPFound(location=location)
 
 
