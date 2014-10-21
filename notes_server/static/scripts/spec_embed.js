20075c8d0445cc42b459153e4a683ab3db092a9b
Renoir Boulanger <renoir@w3.org>
2014-10-21 17:05:04 -0400

    Conditional statement for IE was mistaken

--- notes_server/static/scripts/spec_embed.js
+++ notes_server/static/scripts/spec_embed.js
@@ -2,7 +2,7 @@
   if ( typeof document.querySelectorAll !== 'undefined' ) {
     var s = document.querySelectorAll('link[type="application/annotator+html"]');
 
-    if ( /Trident\//.test(navigator.userAgent) === false && !!s.length ) {
+    if ( /Trident/.test(navigator.userAgent) === true && !!s.length ) {
       var appAttr = s[0].href;
       window.hypothesisConfig = function () {
         return {
